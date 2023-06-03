18b89099275ffde948e453c6df8a75409fc437c8
kmiles32 <kmiles4332@gmail.com>
2023-06-02 17:19:59 -0700

    Start bechdel_test.py

--- final_report.py
+++ final_report.py
@@ -12,27 +12,39 @@ import json
 
 final = pd.read_csv('final_report.csv')
 passing_movies = []
+passing_movies_years = []
 close_pass = []
+close_pass_years = []
 one_pass = []
+one_pass_years = []
 failed = []
+failed_years = []
 missing = []
 
 for ind in final.index:
     if final['rating_y'][ind] == 3.0:
         passing_movies.append(final['title'][ind])
+        passing_movies_years.append(int(final['year_x'][ind]))
     elif final['rating_y'][ind] == 2.0:
         close_pass.append(final['title'][ind])
+        close_pass_years.append(int(final['year_x'][ind]))
     elif final['rating_y'][ind] ==1.0:
         one_pass.append(final['title'][ind])
+        one_pass_years.append(int(final['year_x'][ind]))
     elif final['rating_y'][ind] ==0.0:
         failed.append(final['title'][ind])
+        failed_years.append(int(final['year_x'][ind]))
     else:
         missing.append(final['movie_title'][ind])
 print(f"How many passed: {len(passing_movies)}/250")
+print(f"Average Years for passed: {sum(passing_movies_years)/len(passing_movies_years)}")
 print(f"How many passed 2: {len(close_pass)}/250")
+print(f"Average Years for passed 2: {sum(close_pass_years)/len(close_pass_years)}")
 print(f"How many passed 1: {len(one_pass)}/250")
+print(f"Average Years for passed 1: {sum(one_pass_years)/len(one_pass_years)}")
 print(f"How many failed: {len(failed)}/250")
+print(f"Average Years for failed: {sum(failed_years)/len(failed_years)}")
 print(f"How many are missing: {len(missing)}/250")
-for movie in missing:
+for movie in failed:
     print(movie)
 
